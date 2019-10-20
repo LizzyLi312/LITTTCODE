b7332bc75b85635ea79659167d37873ae8122347
LizzyLi312 <38482151+LizzyLi312@users.noreply.github.com>
2019-10-19 23:29:58 -0700

    Create Combination Sum.java

---
+++ Combination Sum.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+        if(candidates == null || candidates.length == 0) return res;
+        List<Integer> sol = new ArrayList<>();
+        dfs(candidates, target, 0, sol, res, 0);
+        return res;
+    }
+    private void dfs(int[] nums, int target, int level, List<Integer> sol, List<List<Integer>> res, int sum){
+        if(sum == target){  //the result could be generated on every node
+            res.add(new ArrayList<>(sol));
+            return;
+        }
+        if(sum > target || level >= nums.length) return;  //2 base case, need to write seperatly
+        for(int i = level; i < nums.length; i++){
+            sol.add(nums[i]);
+            dfs(nums, target, i, sol, res, sum + nums[i]);
+            sol.remove(sol.size() - 1);
+        }
+    }
+
+}
