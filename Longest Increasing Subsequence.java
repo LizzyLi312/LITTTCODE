347bef403ee9e8b82b330ed4125173ceb083a157
LizzyLi312 <38482151+LizzyLi312@users.noreply.github.com>
2019-10-21 22:51:44 -0700

    Create  Longest Increasing Subsequence.java

---
+++ Longest Increasing Subsequence.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        if(nums == null || nums.length == 0) return 0;
+        if(nums.length < 2) return nums.length;
+        int[] dp = new int[nums.length];
+        int max = 1;
+        for(int i = 0; i < nums.length; i++){
+            dp[i] = 1;
+            for(int j = 0; j < i; j++){
+                if(nums[i] > nums[j]){
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                    max = Math.max(dp[i], max);
+                }
+            }
+        }
+        return max;
+    }
+}
