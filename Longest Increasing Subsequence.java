a1ec5920b9159eecffa23320dcf234e941cfa179
LizzyLi312 <38482151+LizzyLi312@users.noreply.github.com>
2019-10-22 13:07:51 -0700

    Update Longest Increasing Subsequence.java

--- Longest Increasing Subsequence.java
+++ Longest Increasing Subsequence.java
@@ -4,11 +4,11 @@ class Solution {
         if(nums.length < 2) return nums.length;
         int[] dp = new int[nums.length];
         int max = 1;
-        for(int i = 0; i < nums.length; i++){
+        for(int i = 0; i < nums.length; i++){  //visit each number in the array
             dp[i] = 1;
-            for(int j = 0; j < i; j++){
+            for(int j = 0; j < i; j++){  //this loop starts when i > j! so the nums[i] is ahead of nums[j]
                 if(nums[i] > nums[j]){
-                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                    dp[i] = Math.max(dp[i], dp[j] + 1);  //update dp[i]
                     max = Math.max(dp[i], max);
                 }
             }
