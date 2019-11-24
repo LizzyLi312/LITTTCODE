d80167d8e51450ef4049ab14565429069fd40fd8
LizzyLi312 <38482151+LizzyLi312@users.noreply.github.com>
2019-11-24 13:56:19 -0800

    Update Find Common Characters.java

--- Find Common Characters.java
+++ Find Common Characters.java
@@ -2,18 +2,25 @@ class Solution {
     public List<String> commonChars(String[] A) {
         List<String> res = new ArrayList<>();
         if(A == null || A.length == 0) return res;
-        Map<Character, Integer> map = new HashMap<>();
-        for(int i = 0; i < A.length; i++){
-            for(int j = 0; j < A[i].length(); j++){
-                int count = map.getOrDefault(A[i].charAt(j), 0);
-                map.put(A[i].charAt(j), count + 1);
-            }
+        Map<Character, Integer> common= new HashMap<>();
+        for(int i = 0; i < A[0].length(); i++){  //build up the 1st hashmap
+            common.put(A[0].charAt(i), common.getOrDefault(A[0].charAt(i), 0) + 1);
         }
-        for(char ch : map.keySet()){
-            for(int i = 0; i < map.get(ch) / 3; i++){
-                res.add(new String(ch));
+        for(int i = 1; i < A.length; i++){ //find common
+            Map<Character, Integer> temp = new HashMap<>();
+            for(int j = 0; j < A[i].length(); j++){
+                char ch = A[i].charAt(j);
+                if(common.containsKey(ch)) temp.put(ch, Math.min(temp.getOrDefault(ch, 0) + 1, common.get(ch)));
+            }
+            common = temp;
+        }
+        for(char ch : common.keySet()){
+            for(int i = 0; i < common.get(ch); i++){
+                res.add(String.valueOf(ch));  //Returns the string representation of the specific argument
             }
         }
         return res;
     }
 }
+
+//using 2 HashMaps. 1st to store common character from the previous Strings. 2nd one store the common characters from the 1st hashmap and the current string 
