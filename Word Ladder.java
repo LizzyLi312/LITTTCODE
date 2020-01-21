2ddb41b4a66ab7bd173ef5c146c7ee280b45cee3
LizzyLi312 <38482151+LizzyLi312@users.noreply.github.com>
2020-01-21 10:55:17 -0800

    Create Word Ladder.java

---
+++ Word Ladder.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        if(beginWord == null || endWord == null || wordList == null) return -1;  //corner case
+        HashSet<String> wordSet = new HashSet<>();  //de duplicate using hashset
+        for(String word : wordList) wordSet.add(word);
+        Queue<String> queue = new LinkedList<String>();
+        queue.offer(beginWord);
+        int minDis = 1;  //record the distance 
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            while(size-- > 0){
+                String cur = queue.poll();
+                char cc[] = cur.toCharArray();
+                for(int i = 0; i < cc.length; i++){
+                    char temp = cc[i];
+                    for(char c = 'a'; c <= 'z'; c++){
+                        cc[i] = c; 
+                        String str = String.valueOf(cc);
+                        if(c != temp && wordSet.contains(str)){
+                            if(str.equals(endWord)) return minDis + 1;
+                            queue.offer(str);
+                            wordSet.remove(str);
+                        }                           
+                    }
+                    cc[i] = temp;
+                }
+            }
+            minDis++;
+        }
+        return 0;
+    }
+}
+
+//using bfs
