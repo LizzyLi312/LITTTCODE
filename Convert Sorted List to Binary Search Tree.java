1288b2c88d756cc686adafd7a41829f3bff904aa
LizzyLi312 <38482151+LizzyLi312@users.noreply.github.com>
2020-02-21 00:27:47 -0800

    Update Convert Sorted List to Binary Search Tree.java

--- Convert Sorted List to Binary Search Tree.java
+++ Convert Sorted List to Binary Search Tree.java
@@ -3,17 +3,19 @@ class Solution {
         if(head == null) return null;
         return dfs(head, null);
     }
-    private TreeNode dfs(ListNode head, ListNode tail){
+    private TreeNode dfs(ListNode head, ListNode tail){ 
+        if(head == tail) return null;  //if there is nothing in the linkedlist then return null
         ListNode slow = head;
         ListNode fast = head;
-        if(head == tail) return null;
-        while(fast.next != tail && fast.next.next != tail){ //find the middle to be the root since BST is sorted 
+        while(fast.next != tail && fast.next.next != tail){ //find the middle to be the root since BST is sorted
             slow = slow.next;
             fast = fast.next.next;
         }
-        TreeNode root = new TreeNode(slow.val);
+        TreeNode root = new TreeNode(slow.val);  //every parent node should be the middle of the children nodes
         root.left = dfs(head, slow);  //should always be in the left part
         root.right = dfs(slow.next, tail);
         return root;
     }
 }
+
+//the numbers of node at left side and the right side are the same so it can make sure
