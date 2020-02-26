0432437a4270f4505054e8f39bc66250fbd7158c
LizzyLi312 <38482151+LizzyLi312@users.noreply.github.com>
2020-02-26 15:00:17 -0800

    Update Diameter of Binary Tree.java

--- Diameter of Binary Tree.java
+++ Diameter of Binary Tree.java
@@ -4,14 +4,14 @@ class Solution {
         int[] res = new int[1];  
         res[0] = 1;
         diam(root, res);
-        return res[0] - 1;  //since when root == null return 1. so the degree of leaf node is 1, but it should be 0
+        return res[0] - 1;  //the return value is the number of the node, the number of edge = number of node -1 
     }
     private int diam(TreeNode root, int[] res){
         if(root == null) return 0;  //base case
         int right = diam(root.right, res);
         int left = diam(root.left, res);
-        res[0] = Math.max(left + right + 1, res[0]);  //the res so far
-        return Math.max(left, right) + 1;  //the max depth of each branch 
+        res[0] = Math.max(left + right + 1, res[0]);  //the res so far, plus 1 means the root itself 
+        return Math.max(left, right) + 1;  //the max depth of each branch, plus 1 means the root itself 
     }
 }
 
