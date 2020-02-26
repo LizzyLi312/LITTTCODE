f2aeb9907bdb40d8db48f9fc59d5c1fc6567e3ba
LizzyLi312 <38482151+LizzyLi312@users.noreply.github.com>
2020-02-26 14:25:42 -0800

    Update Kth Smallest Element in a BST.java

--- Kth Smallest Element in a BST.java
+++ Kth Smallest Element in a BST.java
@@ -15,6 +15,29 @@ class Solution {
         Integer rightSize = helper(root.right, k - leftSize - 1);
         return rightSize == null ? null : (leftSize + rightSize + 1);  //when the target is at right side return null. Otherwise return the number of node to the parent node
     }
+    //Or use flag, -1 means we found the kth. When we found the kth mark the flag to true
+    //so when either the right side or the left side equals to -1 then we return -1.
+    //else return the number of nodes
+    private int res = 0;
+    private boolean flag = false;
+    public int kthSmallest(TreeNode root, int k) {
+        helper(root, k);
+        return flag == true ? res : -1;
+    }
+    private int helper(TreeNode root, int k){
+        if(root == null) return 0;
+        int leftSize = helper(root.left, k);
+        if(leftSize == -1) return -1;
+        if(leftSize + 1 == k){
+            res = root.val;
+            flag = true;
+            return -1;
+        }
+        int rightSize = helper(root.right, k - leftSize - 1);  // k - leftSize - 1 means except left side node and the root itself 
+        if(rightSize == -1) 
+            return -1;
+        return leftSize + rightSize + 1;
+    }
 }
 
 //BST. SORTED! if the there is no left child then the parent node is def smaller than the right child
