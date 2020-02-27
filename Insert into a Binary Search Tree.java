394a112a24c205eb0db8933b9232132de331214d
LizzyLi312 <38482151+LizzyLi312@users.noreply.github.com>
2020-02-27 10:28:12 -0800

    Update Insert into a Binary Search Tree.java

--- Insert into a Binary Search Tree.java
+++ Insert into a Binary Search Tree.java
@@ -3,8 +3,8 @@ class Solution {
         //using recursion 
         if(root == null) return root;
         if(root.left == null && root.val > val) root.left = new TreeNode(val);
-        if(root.right == null && root.val < val) root.right = new TreeNode(val);  //need to cache since you do not know where the new node should be inserted
-        if(root.val < val) root.right = insertIntoBST(root.right, val);
+        if(root.right == null && root.val < val) root.right = new TreeNode(val); 
+        if(root.val < val) root.right = insertIntoBST(root.right, val); //need to cache since you do not know where the new node should be inserted
         else root.left = insertIntoBST(root.left, val);
         return root;
         //using interation
