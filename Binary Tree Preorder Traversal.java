ccf9e94131306b60a8462975e0a95342a176ed00
LizzyLi312 <38482151+LizzyLi312@users.noreply.github.com>
2020-02-27 11:18:26 -0800

    Update Binary Tree Preorder Traversal.java

--- Binary Tree Preorder Traversal.java
+++ Binary Tree Preorder Traversal.java
@@ -1,4 +1,5 @@
 class Solution {
+    //Using stack to store treenode 
     public List<Integer> preorderTraversal(TreeNode root) {
         List<Integer> res = new ArrayList<>();
         if(root == null) return res;
@@ -14,4 +15,17 @@ class Solution {
     }
 }
 
-//Using stack to store treenode 
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if(root == null) return res;
+        pre(root, res);
+        return res;
+    }
+    private void pre(TreeNode root, List<Integer> res){
+        if(root == null) return;
+        res.add(root.val);
+        pre(root.left, res);
+        pre(root.right, res);
+    }
+}
