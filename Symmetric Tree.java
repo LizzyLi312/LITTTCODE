e4409a23bdc509daef8b9aa509e4243e45a1c9c4
LizzyLi312 <38482151+LizzyLi312@users.noreply.github.com>
2020-02-27 11:07:21 -0800

    Update Symmetric Tree.java

--- Symmetric Tree.java
+++ Symmetric Tree.java
@@ -3,9 +3,9 @@ class Solution {
         if(root == null) return true;
         return isSymmetric(root.left, root.right);
     }
-    private boolean isSymmetric(TreeNode node1, TreeNode node2){
+    private boolean isSymmetric(TreeNode node1, TreeNode node2){ //recursion goal: compare the value of these 2 nodes
         if(node1 == null && node2 == null) return true;
-        if(node1 == null && node2 == null) return false;
+        if(node1 == null || node2 == null) return false;
         if(node1.val != node2.val) return false;  //can not be node1.val == node2.val return true, cause their subtree could not be symmetric tree as well
         return (isSymmetric(node1.left, node2.right) && isSymmetric(node1.right, node2.left));
     }
