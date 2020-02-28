72da0bc6da62ecc312a879a65e99ca0d6c09be57
LizzyLi312 <38482151+LizzyLi312@users.noreply.github.com>
2020-02-28 15:46:28 -0800

    Update Binary Tree Inorder Traversal.java

--- Binary Tree Inorder Traversal.java
+++ Binary Tree Inorder Traversal.java
@@ -6,7 +6,7 @@ class Solution {
         TreeNode cur = root;
         while(cur != null || !stack.empty()){
             if(cur != null){ //when the pointer travers the null below leaf node, them push the value
-                stack.push(cur);
+                stack.push(cur);  //go fine the most left node
                 cur = cur.left;  //go left
             }
             else{
