20fd96e5535af8649924176081c80ab2b8f1887b
LizzyLi312 <38482151+LizzyLi312@users.noreply.github.com>
2020-02-28 14:49:17 -0800

    Update Binary Tree Postorder Traversal.java

--- Binary Tree Postorder Traversal.java
+++ Binary Tree Postorder Traversal.java
@@ -7,9 +7,24 @@ class Solution {
         while(!stack.empty()){
             TreeNode cur = stack.pop();
             res.addFirst(cur.val);  //add value at the first position 
-            if(cur.left != null) stack.push(cur.left);
+            if(cur.left != null) stack.push(cur.left);  //do not need to switch the order of left and right since first in last one. so the left one will be out afte the right. but it will be added before the right since res.add(0, val)
             if(cur.right != null) stack.push(cur.right);
         }
         return res;
+    }
+}
+
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if(root == null) return res;
+        pre(root, res);
+        return res;
+    }
+    private void pre(TreeNode root, List<Integer> res){
+        if(root == null) return;
+        pre(root.left, res);
+        pre(root.right, res);
+        res.add(root.val);
     }
 }
