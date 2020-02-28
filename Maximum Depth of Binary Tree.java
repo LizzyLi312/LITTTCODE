43583e7dd6557e84e1a1c6b741766da3f3812a77
LizzyLi312 <38482151+LizzyLi312@users.noreply.github.com>
2020-02-28 15:54:27 -0800

    Update Maximum Depth of Binary Tree.java

--- Maximum Depth of Binary Tree.java
+++ Maximum Depth of Binary Tree.java
@@ -3,7 +3,7 @@ class Solution {
         if(root == null) return 0;
         int left = maxDepth(root.left);
         int right = maxDepth(root.right);
-        return Math.max(left, right) + 1;  //add depth here
+        return Math.max(left, right) + 1;  //add depth here or add 1 on left or right. the value is returned to the parents node 
     }
 }
 
