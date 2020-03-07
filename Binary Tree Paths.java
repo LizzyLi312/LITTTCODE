58fad852cb3147568f7ead42ba736b89c40d5365
LizzyLi312 <38482151+LizzyLi312@users.noreply.github.com>
2020-03-07 12:20:44 -0800

    Create Binary Tree Paths.java

---
+++ Binary Tree Paths.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<String> binaryTreePaths(TreeNode root) {
+        List<String> res = new ArrayList<>();
+        if(root == null) return res;
+        dfs(root, res, new StringBuilder());
+        return res;
+    }
+    private void dfs(TreeNode root, List<String> res, StringBuilder sol){
+        if(root == null)
+            return;
+        int len = sol.length();  //need to cache the length befroe the new value gets added in 
+        if(root.left == null && root.right == null){  //avoid an extra "->" in the end
+            sol.append(root.val);
+            res.add(sol.toString()); //add to the res when it meets the leaf node
+        }
+        else{
+            sol.append(root.val + "->"); 
+            dfs(root.left, res, sol);
+            dfs(root.right, res, sol);
+            
+        }
+        sol.setLength(len);  //backtracing 
+    }
+}
