5eaa099106cb5567577a4a6b7f5c35b81f736c58
LizzyLi312 <38482151+LizzyLi312@users.noreply.github.com>
2020-03-12 22:26:14 -0700

    Update Binary Tree Paths.java

--- Binary Tree Paths.java
+++ Binary Tree Paths.java
@@ -8,7 +8,7 @@ class Solution {
     private void dfs(TreeNode root, List<String> res, StringBuilder sol){
         if(root == null)
             return;
-        int len = sol.length();  //need to cache the length befroe the new value gets added in 
+        int len = sol.length();  //need to cache the length befroe the new value gets added in. when we process every node, we have a length.
         if(root.left == null && root.right == null){  //avoid an extra "->" in the end
             sol.append(root.val);
             res.add(sol.toString()); //add to the res when it meets the leaf node
@@ -19,6 +19,6 @@ class Solution {
             dfs(root.right, res, sol);
             
         }
-        sol.setLength(len);  //backtracing 
+        sol.setLength(len);  //backtracing. can not use len - 1 since the -> counts to length too
     }
 }
