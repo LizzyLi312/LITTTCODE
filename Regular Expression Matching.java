be077ab16b0a28f9a010afeeffda147a721e65ab
LizzyLi312 <38482151+LizzyLi312@users.noreply.github.com>
2020-03-19 14:09:12 -0700

    Update Regular Expression Matching.java

--- Regular Expression Matching.java
+++ Regular Expression Matching.java
@@ -1,3 +1,4 @@
+//if there are only '.' 2 pointers can solve the prob. But if there are '*' we need dfs to see how many character '*' can offset
 class Solution {
     public boolean isMatch(String s, String p) {
         if(p == null || s == null) return false;
@@ -8,14 +9,16 @@ class Solution {
         if(idxP == lenP) return idxS == lenS;  //do not use idxS == lenS as the condition since there could be * in the end. e.g: "aaa" "a*"
         char c = s.charAt(idxS);
         char b = p.charAt(idxP);
-        if(idxP == lenP - 1 || p.charAt(idxP + 1) != '*'){ //last position of P || the next character is '.' or letter
-            if(idxS < lenS && (b == '.' || b == c)) return dfs(s, p, idxS + 1, idxP + 1);
+        //check non '*'
+        if(idxP == lenP - 1 || p.charAt(idxP + 1) != '*'){ //last position of P since there is no letter after that || the next character is '.' or letter since the * symbol always bundle with the previous one
+            if(idxS < lenS && (b == '.' || b == c)) return dfs(s, p, idxS + 1, idxP + 1);  //before check match we need to check the boundary
             else return false;
         }
+        //check '*'
         else{  //if there is '*' in p
-            int i = idxS - 1;
+            int i = idxS - 1;  //can be knew after the variable's value in dfs determined. since we need to start from idxS. So the while loop need to add an condition i < idxS
             while(i < lenS && (i < idxS || b == '.' || b == s.charAt(i))){ //need while loop since we do not know how many letters in s could be matched 
-                if(dfs(s, p, i + 1, idxP + 2)) return true;
+                if(dfs(s, p, i + 1, idxP + 2)) return true;  //next step: match(i+1, idxP+2) since idxP is a non '*'letter and idxP+1 is a '*' letter.so next one should be the one after these two.
                 i++;
             }
             return false;
