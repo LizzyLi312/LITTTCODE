7085a2bc0abd566d519d353074528cca99c12c9b
LizzyLi312 <38482151+LizzyLi312@users.noreply.github.com>
2020-03-27 14:07:20 -0700

    Update ?Expression Add Operators.java

--- ?Expression Add Operators.java
+++ ?Expression Add Operators.java
@@ -3,8 +3,7 @@
 class Solution {
     public List<String> addOperators(String num, int target) {
         List<String> res = new ArrayList<>();
-        // if(num == null) return res;
-        // dfs(num, target, new StringBuilder(), 0, 0, res, 0);
+        if(num == null) return res;
         dfs(res, num,0 ,0, new StringBuilder(), target);
         return res;
         
@@ -16,10 +15,10 @@ class Solution {
             return;
         }
         if(len == 0) return;
-        int lenSb = sb.length();
-        int val = 0;
+        int lenSb = sb.length(); //need to be cached befroe the dfs starts. can be put in the for loop too 
+        long val = 0; //need to be long 
         for(int i = 0; i < len; i++){
-            val = val * 10 + num.charAt(i) - '0';
+            val = val * 10 + num.charAt(i) - '0'; //there could be no operator sign between 2 numbers
             if(sb.length() != 0){
                 dfs(res, num.substring(i + 1), val, curVal + val, sb.append("+" + val), target);
                 sb.setLength(lenSb);
@@ -32,39 +31,7 @@ class Solution {
                 dfs(res, num.substring(i + 1), val, curVal + val, sb.append(val), target);
                 sb.setLength(lenSb);
             }
-            if(val == 0) break;
+            if(val == 0) break; //last place otherwise cases like this cannot pass: "105" 5 Expected: ["1*0+5","10-5"]. the first one could be missed
         }
     }
-//     private void dfs(String num, int target, StringBuilder sol, long sum, long lastVal, List<String> res, int idx){
-//         int len = num.length();
-//         if(idx == len && sum == target){
-//             res.add(sol.toString());
-//             return;
-//         }
-//         if(idx > len) return;
-//         long val = 0;
-//         int lenSol = sol.length();
-//         for(int i = idx; i < len; i++){
-//             val = 0 * val + (num.charAt(i) - '0');
-//             if(sol.length() == 0){
-//                 sol.append(val);
-//                 dfs(num, target, sol, sum + val, val, res, i + 1);
-//                 sol.setLength(sol.length() - 1);
-//             }
-//             else{
-//                 sol.append("+" + val);
-//                 dfs(num, target, sol, sum + val, val, res, i + 1);
-//                 sol.setLength(lenSol);
-                
-//                 sol.append("-" + val);
-//                 dfs(num, target, sol, sum - val, -val, res, i + 1);
-//                 sol.setLength(lenSol);
-                
-//                 sol.append("*" + val);
-//                 dfs(num, target, sol, (sum - lastVal) + lastVal * val, lastVal * val, res, i + 1);
-//                 sol.setLength(lenSol);
-//             }
-//             if(val != 0) break;
-//         }
-//     }
 }
