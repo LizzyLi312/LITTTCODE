b412f632f8cb367bad562e1c7b4c07f3c3221afa
LizzyLi312 <38482151+LizzyLi312@users.noreply.github.com>
2020-03-29 15:29:44 -0700

    Create Maximum Product Subarray.java

---
+++ Maximum Product Subarray.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxProduct(int[] nums) {
+        if(nums == null || nums.length == 0) return 0;
+        int max = nums[0]; //position[i] max value so far
+        int minL = nums[0], maxL = nums[0];
+        for(int i = 1; i < nums.length; i++){
+            int copy = minL;
+            minL = Math.min(Math.min(minL * nums[i], maxL * nums[i]), nums[i]); //for the negative integers. e.g.[-2,3,-4]
+            maxL = Math.max(Math.max(copy * nums[i], maxL * nums[i]), nums[i]);
+            max = Math.max(max, maxL);
+        }
+        return max;
+    }
+}
