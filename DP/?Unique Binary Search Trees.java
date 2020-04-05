6ebcdd3fbc7fd3157a3bd08295039931225cf8a8
LizzyLi312 <38482151+LizzyLi312@users.noreply.github.com>
2020-04-05 00:05:08 -0700

    Create ?Unique Binary Search Trees.java

---
+++ DP/?Unique Binary Search Trees.java
@@ -0,0 +1,15 @@
+class Solution {
+    private int res = 0;
+    public int numTrees(int n) {
+        int[] memo = new int[n + 1];
+        memo[0] = 1;
+        for(int len = 1; len <= n; len++){
+            int count = 0;
+            for(int i = 1; i <= len; i++){
+                count += memo[i - 1] * memo[len - i];
+            }
+            memo[len] = count;
+        }
+        return memo[n];
+    }
+}
