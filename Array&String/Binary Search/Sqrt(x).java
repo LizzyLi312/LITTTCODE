2cd7f281ee4e823a9715516ff2aef9abe0374825
LizzyLi312 <38482151+LizzyLi312@users.noreply.github.com>
2020-04-12 14:01:17 -0700

    Update Sqrt(x).java

--- Array&String/Binary Search/Sqrt(x).java
+++ Array&String/Binary Search/Sqrt(x).java
@@ -1,7 +1,7 @@
 class Solution {
     public int mySqrt(int x) {
         if(x == 0) return 0;
-        int start = 1, end = Integer.MAX_VALUE;
+        int start = 1, end = x;
         while(start + 1 < end){
             int mid = start + (end - start) / 2;
             if(mid  == x / mid) return mid; //do not use mid * mid since it could cause integer overflow. and we just need the decimal part
