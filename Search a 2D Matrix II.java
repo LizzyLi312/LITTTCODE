931190c2ee34685787e7883034797a851c8e4112
LizzyLi312 <38482151+LizzyLi312@users.noreply.github.com>
2020-04-17 16:25:54 -0700

    Update Search a 2D Matrix II.java

--- Search a 2D Matrix II.java
+++ Search a 2D Matrix II.java
@@ -1,29 +1,13 @@
-//Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
-
-//Integers in each row are sorted in ascending from left to right.
-//Integers in each column are sorted in ascending from top to bottom.
-
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         if(matrix == null || matrix.length == 0 || matrix[0] == null || matrix[0].length == 0) return false;
-        int row = matrix.length;
-        int col = matrix[0].length;
-        for(int i = 0; i < row; i++){
-            int left = 0;
-            int right = col - 1;
-            boolean flag = false;
-            while(left <= right){
-                int mid = left + (right - left) / 2;
-                if(target == matrix[i][mid]) flag = true;
-                if(target > matrix[i][mid]) left = mid + 1;
-                else right = mid - 1;
-            }
-            if(flag == true) return true;
-            else continue;
+        int row = matrix.length, col = matrix[0].length;
+        int rowIdx = row - 1, colIdx = 0;
+        while(rowIdx >= 0 && colIdx < col){
+            if(matrix[rowIdx][colIdx] == target) return true;
+            else if(matrix[rowIdx][colIdx] < target) colIdx++;
+            else rowIdx--;
         }
         return false;
     }
 }
-
-//binary search for each row
-//time: O(mlogn)    space: O(1)
