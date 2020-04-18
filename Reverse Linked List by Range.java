32a3fd015461f0945f934aa32c375b41c760be05
LizzyLi312 <38482151+LizzyLi312@users.noreply.github.com>
2020-04-18 16:26:43 -0700

    Update Reverse Linked List by Range.java

--- Reverse Linked List by Range.java
+++ Reverse Linked List by Range.java
@@ -1,41 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
 class Solution {
     public ListNode reverseBetween(ListNode head, int m, int n) {
         if(head == null || head.next == null || m == n) return head;
-        //create dummy node
         ListNode dummy = new ListNode(0);
         dummy.next = head;
-        //find the start node and the node before it
-        ListNode cur = head, pre = dummy;
-        for(int i = 1; i < m; i++){
-            pre = cur;
-            cur = cur.next;
-        }
-        pre.next = null;
-        ListNode start = cur, cur2 = cur.next;
-        //find the end node and the node after it
-        for(int i = 0; i < n - m; i++){
-            start = cur2;
-            cur2 = cur2.next;
-        }
-        start.next = null;
-        //reverse
-        ListNode temp_newHead = reverse(cur);
-        //link
-        pre.next = temp_newHead;
-        cur.next = cur2;
-        return dummy.next;
+        ListNode firstNode = findNode(dummy, m - 1); //prev1
+        ListNode end = findNode(dummy, n); //prev2
+        ListNode start = firstNode.next; //cur1
+        ListNode secondNode = end.next; //cur2
         
+        firstNode.next = null;
+        end.next = null;
+        
+        ListNode newHead = reverse(start);
+        firstNode.next = newHead;
+        start.next = secondNode;
+        return dummy.next;
+    }
+    private ListNode findNode(ListNode head, int count){
+        while(count-- > 0){
+            head = head.next;
+        }
+        return head;
     }
     private ListNode reverse(ListNode head){
-        ListNode prev = null;
-        ListNode cur = head;
-        ListNode next = null;
+        ListNode prev = null, cur = head, next = null;
         while(cur != null){
             next = cur.next;
             cur.next = prev;
             prev = cur;
             cur = next;
-        }   
+        }
         return prev;
     }
 }
