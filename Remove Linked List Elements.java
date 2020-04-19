ab26285cda88aa6943dd911902db0c1e76d18961
LizzyLi312 <38482151+LizzyLi312@users.noreply.github.com>
2020-04-18 19:23:58 -0700

    Update Remove Linked List Elements.java

--- Remove Linked List Elements.java
+++ Remove Linked List Elements.java
@@ -1,7 +1,7 @@
 class Solution {
     public ListNode removeElements(ListNode head, int val) {
         if(head == null) return head;
-        ListNode dummy = new ListNode(0);
+        ListNode dummy = new ListNode(0); //need to add dummy node since if there are duplicates and they both need to be deleted
         dummy.next = head;
         ListNode cur = head;
         ListNode pre = dummy;  //need to keep 2 variables
