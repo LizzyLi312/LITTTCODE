ac01c3935985bed2817e2c829d6bd92c82958749
LizzyLi312 <38482151+LizzyLi312@users.noreply.github.com>
2020-04-26 14:22:11 -0700

    Update Subtree of Another Tree.java

--- Subtree of Another Tree.java
+++ Subtree of Another Tree.java
@@ -1,8 +1,9 @@
+//similar as same tree
 class Solution {
     public boolean isSubtree(TreeNode s, TreeNode t) {
-        if(s == null) return false;
+        if(s == null && t != null) return false;
         if(isSameTree(s, t)) return true;
-        return isSubtree(s.left,t) || isSubtree(s.right, t);
+        return isSubtree(s.left,t) || isSubtree(s.right, t);  //in this way find the node
     }
     private boolean isSameTree(TreeNode left, TreeNode right){  //use the same tree to find the start treenode
         if(left == null && right == null) return true;
