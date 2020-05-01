6329157e65478bac696e2490caa54f0b2e3b21e0
LizzyLi312 <38482151+LizzyLi312@users.noreply.github.com>
2020-05-01 16:56:11 -0700

    Create Reverse Words in a String.java

---
+++ Array&String/Reverse Words in a String.java
@@ -0,0 +1,74 @@
+//reverse the whole string then reverse each word
+class Solution {
+    public String reverseWords(String s) {
+        if(s == null || s.length() == 0) return s;
+        String str = s.trim(); //cut the tailing and heading spaces
+        String rever = reverse(str); //reverse the whole sentence
+        String words = reverseWord(rever); //reverse each word
+        return clean(words.toCharArray());
+    }
+    private String clean(char[] s){ //use 2 pointers. inplace remove extra space 
+        int i = 0, j = 0, n = s.length;
+        while(j < n){
+            while(j < n && s[j] != ' ') s[i++] = s[j++]; //keep the non space characters
+            while(j < n && s[j] == ' ') j++; //find the next non space character
+            if(j < n) s[i++] = ' '; //keep one space
+        }
+        return new String(s).substring(0, i);
+    }
+    private String reverse(String s){
+        if(s.length() == 0) return "";
+        char[] arr = s.toCharArray();
+        int left = 0, right = arr.length - 1;
+        while(left < right){
+            swap(arr, left++, right--);
+        }
+        return new String(arr);
+    }
+    private String reverseWord(String s){
+        if(s.length() == 0) return "";
+        char[] arr = s.toCharArray();
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] == ' ') continue;
+            else if(arr[i] != ' '){ //find the start & end for each word
+                int j = i + 1;
+                while(j < arr.length && arr[j] != ' '){
+                    j++;
+                }
+                int left = i, right = j - 1;
+                while(left < right){ //reverse each word
+                    swap(arr, left++, right--);
+                }
+                i = j;
+            }
+        }
+        return new String(arr);
+    }
+    private void swap(char[] arr, int i, int j){
+        char temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
+
+//using split and trim. cannot handle when there is multiple spaces between 2 characters
+class Solution {
+    public String reverseWords(String s) {
+        if(s == null || s.length() == 0) return s;
+        String[] tokens = s.split(" ");
+        int left = 0, right = tokens.length - 1;
+        while(left < right){
+            String temp = tokens[left];
+            tokens[left] = tokens[right];
+            tokens[right] = temp;
+            left++;
+            right--;
+        }
+        StringBuilder sb = new StringBuilder();
+        for(String token : tokens){
+            sb.append(token + " ");
+        }
+        String res = sb.toString();
+        return res.trim();
+    }
+}
