f09319843587410793ee544b814c1871276664da
LizzyLi312 <38482151+LizzyLi312@users.noreply.github.com>
2020-05-11 12:39:43 -0700

    Update Word Break II.java

--- DFS&BFS/Word Break II.java
+++ DFS&BFS/Word Break II.java
@@ -2,7 +2,7 @@ class Solution {
     public List<String> wordBreak(String s, List<String> wordDict) {
         List<String> res = new ArrayList<>();
         if(s == null || s.length() == 0) return res;
-        boolean[] memo = new boolean[s.length() + 1];  //pruning
+        boolean[] memo = new boolean[s.length() + 1];  //since it contains empty string so we need len + 1
         Arrays.fill(memo, true);
         dfs(s, wordDict, res, 0, new StringBuilder(), memo);
         return res;
@@ -24,7 +24,7 @@ class Solution {
                 sol.setLength(prevLen);
             }
             //or
-            if(set.contains(s.substring(idx, i + 1)) && memo[i + 1]){
+            if(set.contains(s.substring(idx, i + 1)) && memo[i + 1]){ //pruning : [idx, len - 1) is in the dictionary
                 if(len == 0) sol.append(s.substring(idx, i + 1));
                 else sol.append(" " + s.substring(idx, i + 1));
                 dfs(s, set, i + 1, res, sol, memo);
