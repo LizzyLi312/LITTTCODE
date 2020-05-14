9bd84dc5f0d8fccc0acbb0a9f2ef1b4a0c964ce5
LizzyLi312 <38482151+LizzyLi312@users.noreply.github.com>
2020-05-14 15:31:19 -0700

    Update Binary Tree Maximum Path Sum.java

--- Binary Tree Maximum Path Sum.java
+++ Binary Tree Maximum Path Sum.java
@@ -1,18 +1,16 @@
 class Solution {
-    private int res = Integer.MIN_VALUE;  //otherwise the coner case like: [-3] would not work 
+    private int res = Integer.MIN_VALUE;
     public int maxPathSum(TreeNode root) {
-        helper(root);
+        if(root == null) return 0;
+        max(root);
         return res;
     }
-    private int helper(TreeNode root)
-    {
+    private int max(TreeNode root){
         if(root == null) return 0;
-        int left = helper(root.left);
-        int right = helper(root.right);
-        int val = Math.max(left + root.val, Math.max(right + root.val, root.val));  //each node has three route: root itself, root + right, root + left. find the max one
-        res = Math.max(res, Math.max(val, left + right + root.val));
-        return val;  //return the max route to the prev node
+        int leftSum = max(root.left);
+        int rightSum = max(root.right);
+        int curMax = Math.max(root.val + leftSum + rightSum, Math.max(Math.max(leftSum + root.val, rightSum + root.val), root.val)); //
+        res = Math.max(res, Math.max(curMax, root.val + leftSum + rightSum)); //root.val + leftSum + rightSum can not be returned as a curMax. since it will be a closed route
+        return curMax;
     }
 }
-
-//should clarify: negtative or positive?
