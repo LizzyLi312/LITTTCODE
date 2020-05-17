d9094effef126a76d0197b04dfbaf3e572eadf6a
LizzyLi312 <38482151+LizzyLi312@users.noreply.github.com>
2020-05-17 12:40:10 -0700

    Create ?Median of Two Sorted Arrays.java

---
+++ Array&String/?Median of Two Sorted Arrays.java
@@ -0,0 +1,18 @@
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int len1 = nums1.length;
+        int len2 = nums2.length;
+        int len = len1 + len2;
+        if(len % 2 == 0) return (helper(nums1, nums2, 0, 0, len / 2) + helper(nums1, nums2, 0, 0, len / 2 + 1)) / 2.0;
+        else return helper(nums1, nums2, 0, 0, len / 2 + 1);
+    }
+    private int helper(int[] nums1, int[] nums2, int start1, int start2, int k){
+        if(start1 >= nums1.length) return nums2[start2 + k - 1];
+        if(start2 >= nums2.length) return nums1[start1 + k - 1];
+        if(k == 1) return Math.min(nums1[start1], nums2[start2]);
+        int val1 = start1 + k / 2 - 1 < nums1.length ? nums1[start1 + k/2 - 1] : Integer.MAX_VALUE;
+        int val2 = start2 + k / 2 - 1 < nums2.length ? nums2[start2 + k/2 - 1] : Integer.MAX_VALUE;
+        if(val1 < val2) return helper(nums1, nums2, start1 + k / 2 - 1 + 1, start2, k - k/2);
+        else return helper(nums1, nums2, start1, start2 + k / 2 - 1 + 1, k - k /2);
+    }
+}
