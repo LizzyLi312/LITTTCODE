72f721521d388351e7a868040437efd6a6ece70f
LizzyLi312 <38482151+LizzyLi312@users.noreply.github.com>
2020-05-23 22:49:49 -0700

    Update and rename Find Largest Value in Each Tree Row.java to Tree/Find Largest Value in Each Tree Row.java

--- Tree/Find Largest Value in Each Tree Row.java
+++ Tree/Find Largest Value in Each Tree Row.java
@@ -1,12 +1,4 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
+//bfs
 class Solution {
     public List<Integer> largestValues(TreeNode root) {
         List<Integer> res = new ArrayList<>();
@@ -27,3 +19,21 @@ class Solution {
         return res;
     }
 }
+//time: O(n)    space:O(n)
+
+//dfs
+class Solution {
+    public List<Integer> largestValues(TreeNode root) {
+        List<Integer>  res = new ArrayList<Integer>();
+        helper(root, res, 0);
+        return res;
+    }
+    private void helper(TreeNode root, List<Integer> res, int d){
+        if(root == null) return;
+        if(d == res.size()) res.add(root.val);
+        else res.set(d, Math.max(res.get(d), root.val));
+        helper(root.left, res, d + 1);
+        helper(root.right, res, d + 1);
+    }
+}
+//time: O(n)    space:O(logn)
