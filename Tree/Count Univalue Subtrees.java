4571e9f5a45df1a31052768cf943f6f8c5246557
LizzyLi312 <38482151+LizzyLi312@users.noreply.github.com>
2020-06-11 00:03:29 -0700

    Create Count Univalue Subtrees.java

---
+++ Tree/Count Univalue Subtrees.java
@@ -0,0 +1,18 @@
+class Solution {
+    private int res = 0;
+    public int countUnivalSubtrees(TreeNode root) {
+        if(root == null) return 0;
+        find(root, -1);
+        return res;
+    }
+    private boolean find(TreeNode root, int val){
+        if(root == null) return true;
+        boolean left = find(root.left, root.val);
+        boolean right = find(root.right, root.val);
+        if(left && right){
+            res++;
+            return root.val == val; //compare the root's val and root's root's value 
+        }
+        else return false;
+    }
+}
