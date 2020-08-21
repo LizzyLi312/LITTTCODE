e1b211f6a1574ca1580ecf90e695179e56161fa4
LizzyLi312 <38482151+LizzyLi312@users.noreply.github.com>
2020-08-20 21:04:31 -0700

    Update Binary Tree Maximum Path Sum.java

--- Binary Tree Maximum Path Sum.java
+++ Binary Tree Maximum Path Sum.java
@@ -9,7 +9,7 @@ class Solution {
         if(root == null) return 0;
         int leftSum = max(root.left);
         int rightSum = max(root.right);
-        int curMax = Math.max(root.val + leftSum + rightSum, Math.max(Math.max(leftSum + root.val, rightSum + root.val), root.val)); //
+        int curMax = Math.max(Math.max(Math.max(leftSum + root.val, rightSum + root.val), root.val)); 
         res = Math.max(res, Math.max(curMax, root.val + leftSum + rightSum)); //root.val + leftSum + rightSum can not be returned as a curMax. since it will be a closed route
         return curMax; //cannot return the res since if the res is comes from root.val + leftSum + rightSum. then after we added more element then it is wrong route 
     }
