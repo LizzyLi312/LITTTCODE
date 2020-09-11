26aafa50eff25279d678b2cfac33978021e1f983
LizzyLi312 <38482151+LizzyLi312@users.noreply.github.com>
2020-09-11 14:23:54 -0700

    Update Inorder Successor in BST.java

--- Tree/Inorder Successor in BST.java
+++ Tree/Inorder Successor in BST.java
@@ -1,12 +1,3 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
 class Solution {
     public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
         if(root == null || p == null) return null;
@@ -17,9 +8,33 @@ class Solution {
             }
             else{ //the last update value should be the last one who is larger than p
                 prev = cur;
-                cur = cur.left;
+                cur = cur.left; //cur.left < cur. so the diff between p is def smaller than cur
             }
         }
         return prev;
     }
 }
+
+//iterator inorder 
+class Solution {
+    public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
+        if(root == null) return null;
+        TreeNode cur = root;
+        boolean flag = false;
+        Stack<TreeNode> stack = new Stack<>();
+        while(!stack.empty() || cur != null){
+            if(cur != null){
+                stack.push(cur);
+                cur = cur.left;
+            } 
+            else{
+                cur = stack.pop();
+                if(cur.val == p.val) flag = true;
+                if(flag == true && cur.val > p.val) return cur;
+                cur = cur.right;
+            }
+        }
+        return null;
+    }
+}
+
