762d1a18e1923d14087e02b43ce16ad65508c71b
LizzyLi312 <38482151+LizzyLi312@users.noreply.github.com>
2020-09-11 16:38:44 -0700

    Update Largest BST Subtree.java

--- Largest BST Subtree.java
+++ Largest BST Subtree.java
@@ -1,18 +1,5 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+//use the property of bst. compare the left subtree and right subtree's value
+//so make a wrapper 
 class Solution {
     private class Result{
         public int min, max, size;
