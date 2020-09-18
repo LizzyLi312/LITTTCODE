0b64176decc0f73f8a911dcbf65a6822c7c64632
LizzyLi312 <38482151+LizzyLi312@users.noreply.github.com>
2020-09-17 22:14:27 -0700

    Update Longest Valid Parentheses.java

--- DP/Longest Valid Parentheses.java
+++ DP/Longest Valid Parentheses.java
@@ -38,5 +38,26 @@ class Solution {
         }
         return max;
     }
-    
 }
+
+//stack solution
+class Solution {
+    public int longestValidParentheses(String s) {
+        int max = 0;
+        Stack<Integer> stack = new Stack<>();
+        int left = -1;
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == '(') stack.push(i);
+            else{
+                if(stack.isEmpty()) left = i;
+                else{
+                    stack.pop();
+                    if(stack.isEmpty()) max = Math.max(max, i - left);
+                    else max = Math.max(max, i - stack.peek());
+                }
+            }
+        }
+        return max;
+    }
+}
+//time: O(n)
