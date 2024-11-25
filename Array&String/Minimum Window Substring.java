5fb48119d6ac294b81528f4c17ed66c536de9eae
buildstring <70457037+buildstring@users.noreply.github.com>
2024-11-25 00:00:10 -0800

    Update Minimum Window Substring.java

--- Array&String/Minimum Window Substring.java
+++ Array&String/Minimum Window Substring.java
@@ -74,3 +74,46 @@ class Solution {
     }
 }
 //time: O(n)
+
+
+//solution2 using int[] to mock hashmap
+class Solution {
+    public String minWindow(String s, String t) {
+        if (s == null || s.length() == 0) return "";
+
+        int n = s.length(), m = t.length();
+        if (n < m) return "";
+        int[] map = new int[128];
+        for (int i = 0; i < m; i++) {
+            map[t.charAt(i)]++;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(s);
+        sb.append("#");
+
+        String res = sb.toString();
+
+        int cnt = 0, l = 0;
+        for (int r = 0; r < n; r++) {
+            char c = s.charAt(r);
+            
+            if (map[c] > 0) {    
+                if (map[c] >= 0) cnt++;
+            }
+            map[c] -= 1;
+
+            while (cnt == m && l < n) { // when cnt == m the res could have duplicate t's character in s's substring
+                if (res.length() > r - l + 1) {
+                    res = s.substring(l, r + 1);
+                }
+                if (map[s.charAt(l)] == 0) { // if a character in t has neg val that means we have duplicates in s's substring 
+                    cnt--;
+                }
+                map[s.charAt(l)]++;
+                l++;
+            }
+        }
+        return res.length() == n + 1 ? "" : res;
+    }
+}
