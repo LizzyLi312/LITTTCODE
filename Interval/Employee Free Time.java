4d042137cc540cab39734c52ecc374773f8d6020
buildstring <70457037+buildstring@users.noreply.github.com>
2024-11-29 16:55:01 -0800

    Update Employee Free Time.java

--- Interval/Employee Free Time.java
+++ Interval/Employee Free Time.java
@@ -12,46 +12,28 @@ class Interval {
     }
 };
 */
-//solution2
-
-
-
+//solution2 O(nlogn)
 class Solution {
-    class EndPoint implements Comparable<EndPoint>{
-        int val;
-        boolean isStart;
-        public EndPoint(int val, boolean isStart){
-            this.val = val;
-            this.isStart = isStart;
-        }
-        @Override
-        public int compareTo(EndPoint that){
-            if(this.val != that.val) return this.val - that.val;
-            else return this.isStart ? 1 : -1;
-        }
-    }
     public List<Interval> employeeFreeTime(List<List<Interval>> schedule) {
-        if(schedule == null) return new ArrayList<>();
-        List<EndPoint> eps = new ArrayList<>();
-        for(List<Interval> list : schedule){
-            for(Interval itv : list){
-                eps.add(new EndPoint(itv.start, true));
-                eps.add(new EndPoint(itv.end, false));
+        // 1, 1, 2*, 3*, 4, 5, 6*, 10*
+        //1, 2, 2, 3*, 4*, 5*, 6, 7*, 9, 12*
+        List<int[]> diff = new ArrayList<>();
+        for (List<Interval> s : schedule) {
+            for (Interval i : s) {
+                diff.add(new int[]{i.start, 1});
+                diff.add(new int[]{i.end, -1});
             }
         }
-        Collections.sort(eps);
+        Collections.sort(diff, (o1, o2) -> (o1[0] != o2[0] ? o1[0] - o2[0] : o2[1] - o1[1])); // need to calculate start first since the other employee will be occupied 
+
+        int sum = 0, start = -1;
         List<Interval> res = new ArrayList<>();
-        int cnt = 0;
-        int start = Integer.MIN_VALUE, end = 0;
-        for(EndPoint ep : eps){
-            if(ep.isStart){
-                if(cnt++ == 0){ //means new interval begin 
-                    end = ep.val;
-                    if(start > Integer.MIN_VALUE && start < end) res.add(new Interval(start, end)); 
-                }
-            }
-            else{
-                if(--cnt == 0) start = ep.val; //when the count back to 0 means all interval ends. so should be the start of thr rest 
+        for (int[] d : diff) {
+            sum += d[1];
+            if (sum == 0) start = d[0];
+            if (start != -1 && sum == 1) {
+                res.add(new Interval(start, d[0]));
+                start = -1;
             }
         }
         return res;
