10d23a23f8a8b65bf6771993fc49162c16198313
buildstring <70457037+buildstring@users.noreply.github.com>
2024-12-09 12:43:39 -0800

    Update Swap Nodes in Pairs.java

--- Swap Nodes in Pairs.java
+++ Swap Nodes in Pairs.java
@@ -1,3 +1,4 @@
+// recursion 
 class Solution {
     public ListNode swapPairs(ListNode head) {
         if(head == null || head.next == null) return head;
@@ -6,5 +7,27 @@ class Solution {
         ListNode newHead = head.next;
         head.next = postHead;
         return newHead;
+    }
+}
+
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null) return head;
+
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        ListNode prev = dummy;
+        while (prev.next != null && prev.next.next != null) {
+            ListNode first = prev.next;
+            ListNode second = prev.next.next;
+
+            first.next = second.next;
+            second.next = first;
+            prev.next = second;
+
+            prev = first;
+        }
+        return dummy.next;
+
     }
 }
