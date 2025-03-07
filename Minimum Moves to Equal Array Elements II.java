beb9582a7fb2007bc41c7fa5634ea4e197eb8fa9
LizzyLi312 <38482151+LizzyLi312@users.noreply.github.com>
2025-03-06 23:50:21 -0800

    Create Minimum Moves to Equal Array Elements II.java

---
+++ Minimum Moves to Equal Array Elements II.java
@@ -0,0 +1,43 @@
+// find the median by using quick select, finding the smallest k / 2
+class Solution {
+    public int minMoves2(int[] nums) {
+        if (nums == null || nums.length <= 1) return 0;
+
+        int med = quickSelect(nums, 0, nums.length - 1, nums.length / 2);
+        int res = 0;
+        for (int num : nums) {
+            res += Math.abs(num - med);
+        }
+        return res;
+    }
+
+    private int quickSelect(int[] nums, int left, int right, int k) { // find a pivot and compare the pivot's index with k
+        if (left == right) return nums[left];
+
+        int pivot = left + (right - left) / 2;
+
+        pivot = partition(nums, left, right, pivot); 
+
+        if (pivot == k) return nums[k];
+        else if (pivot < k) return quickSelect(nums, pivot + 1, right, k);
+        return quickSelect(nums, left, pivot - 1, k);
+    }
+
+    private int partition(int[] nums, int left, int right, int p) {
+        int pivot = nums[p];
+
+        swap(nums, p, right);
+        
+        int j = left;
+        for (int i = left; i <= right; i++) {
+            if (nums[i] <= pivot) swap(nums, i, j++);
+        }
+        return j - 1;
+    }
+
+    private void swap(int[] nums, int a, int b) {
+        int t = nums[a];
+        nums[a] = nums[b];
+        nums[b] = t;
+    }
+}
