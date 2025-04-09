acd7f346f1c09e7da25cf7f5eadee8cc07ecf187
LizzyLi312 <38482151+LizzyLi312@users.noreply.github.com>
2025-04-09 00:50:09 -0700

    Create Remove All Adjacent Duplicates in String II.java

---
+++ Array&String/Remove All Adjacent Duplicates in String II.java
@@ -0,0 +1,19 @@
+// 2 pointers, [0, i] result, [i, j]
+class Solution {
+    public String removeDuplicates(String s, int k) {
+        if (s == null || s.length() < k) return s;
+
+        char[] res = s.toCharArray();
+        int l = 0; 
+        int[] cnt = new int[s.length()];
+
+        for (int r = 0; r < s.length(); r++) {
+            res[l] = res[r];
+            if (l > 0 && res[r] == res[l - 1]) cnt[l] = cnt[l - 1] + 1;
+            else cnt[l] = 1;
+            if (cnt[l] == k) l = l - k;
+            l++;
+        }
+        return new String(res, 0, l);
+    }
+}
