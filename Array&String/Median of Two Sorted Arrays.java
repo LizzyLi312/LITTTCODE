ba7f4b43d1a2dfe063b4ab142ad5a4e863f7b94d
LizzyLi312 <38482151+LizzyLi312@users.noreply.github.com>
2025-06-24 16:56:24 -0700

    Update and rename ?Median of Two Sorted Arrays.java to Median of Two Sorted Arrays.java

--- Array&String/Median of Two Sorted Arrays.java
+++ Array&String/Median of Two Sorted Arrays.java
@@ -10,9 +10,11 @@ class Solution {
         if(start1 >= nums1.length) return nums2[start2 + k - 1];
         if(start2 >= nums2.length) return nums1[start1 + k - 1];
         if(k == 1) return Math.min(nums1[start1], nums2[start2]);
+        
         int val1 = start1 + k / 2 - 1 < nums1.length ? nums1[start1 + k/2 - 1] : Integer.MAX_VALUE;
         int val2 = start2 + k / 2 - 1 < nums2.length ? nums2[start2 + k/2 - 1] : Integer.MAX_VALUE;
-        if(val1 < val2) return helper(nums1, nums2, start1 + k / 2 - 1 + 1, start2, k - k/2);
+        // compare val1 and val2 discard the smaller part since none of them could be the kth smallest
+        if(val1 < val2) return helper(nums1, nums2, start1 + k / 2 - 1 + 1, start2, k - k / 2); // +1 since this will be a new start
         else return helper(nums1, nums2, start1, start2 + k / 2 - 1 + 1, k - k /2);
     }
 }
