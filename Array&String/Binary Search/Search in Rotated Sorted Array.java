2d2f83abd6361c325be9a6aa9605e2443bbb589a
LizzyLi312 <38482151+LizzyLi312@users.noreply.github.com>
2025-06-26 15:10:35 -0700

    Update Search in Rotated Sorted Array.java

--- Array&String/Binary Search/Search in Rotated Sorted Array.java
+++ Array&String/Binary Search/Search in Rotated Sorted Array.java
@@ -1,21 +1,30 @@
 class Solution {
     public int search(int[] nums, int target) {
-        if(nums == null || nums.length == 0) return -1;
-        int left = 0, right = nums.length - 1;
-        while(left <= right){
-            int mid = left + (right - left) / 2;
-            if(nums[mid] == target) return mid;
-            if(nums[left] == target) return left;
-            if(nums[right] == target) return right;
-            if(nums[mid] > nums[left]){
-                if(nums[left] < target && target < nums[mid]) right = mid - 1;
-                else left = mid + 1;
-            }
-            else{
-                if(nums[mid] < target && target < nums[right]) left = mid + 1;
-                else right = mid - 1;
-            }
+        if (nums == null || nums.length == 0) return -1;
+
+        int l = 0, r = nums.length - 1;
+        // find pivot - pivot should be the last element that is bigger than nums[0] since if we're looking for the 1st smaller element it would be more things to consider if the arr did not get rotated 
+        while (l < r) {
+            int mid = l + (r - l + 1) / 2;
+            if (nums[mid] < nums[0]) r = mid - 1;
+            else l = mid;
         }
-        return -1;
+
+        int pivot = r;
+
+        if (target >= nums[0]) {
+            l = 0;
+        }
+        else {
+            l = pivot + 1;
+            r = nums.length - 1;
+        }
+        while (l < r) {
+            int mid = l + (r - l) / 2;
+            if (nums[mid] == target) return mid;
+            if (nums[mid] < target) l = mid + 1;
+            else r = mid;
+        }
+        return nums[r] == target ? r : -1;
     }
 }
